1d8f2ae65d9c3fc7d0b44da750f04629598ba4a1
Muhammad Talha <talhajoiyamuhammad.com>
2024-11-11 00:39:06 +0500

    linked list all first type with collection framework also

--- linkedlist/tt.java
+++ linkedlist/tt.java
@@ -42,44 +42,128 @@
 // }
 
 
+// class LL{
+//     private int size;
+//     LL(){
+//         this.size=0;
+//     }
+//     Node head;
+//     class Node{
+//         String data;
+//         Node next;
+//         Node(String data){
+//             this.data=data;
+//             this.next=null;
+//             size++;
+//         }
+//     }
+    
+//     public void addFirst(String data){
+//         Node newNode=new Node(data);
+//         if(head==null){
+//             head=newNode;
+//             return;
+//         }
+//         newNode.next=head;
+//         head=newNode;
+//     }
+    
+
+//     public void addLast(String data){
+//         Node newNode=new Node(data);
+//         if(head==null){
+//             newNode=head;
+//             return;
+//         }
+//         Node curNode=head;
+//         while(curNode.next!=null){
+//             curNode=curNode.next;
+//         }
+//         curNode.next=newNode;
+//     }
+//     public void printList(){
+//         if(head==null){
+//             System.out.println("list is empty");
+//             return;
+//         }
+//         Node currNode=head;
+//         while(currNode!=null){
+//             System.out.print(currNode.data+"-->");
+//             currNode=currNode.next;
+//         }
+//         System.out.println("null");
+//     }
+
+//     // DeleteFirst|DeleteLast
+//     public void DeleteFirst(){
+//         if(head==null){
+//             System.out.println("Linklist is empty");
+//             return;
+//         }
+//         size--;
+//         head=head.next;
+//     }
+//     public void DeleteLast(){
+//         if(head==null){
+//             System.out.println("linklist is empty");
+//             return;
+//         }
+//         size--;
+//         if(head.next==null){
+//             head=null;
+//             return;
+//         }
+//         Node secondLastNode=head;
+//         Node LastNode=head.next;
+//         while (LastNode.next!=null) {
+//             secondLastNode=secondLastNode.next;
+//             LastNode=LastNode.next;
+//         }
+//         secondLastNode.next=null;
+//     }
+
+//     public int getSize(){
+//         return size;
+//     }
+//     public static void main(String arg[]){
+//         LL list=new LL();
+//         list.addFirst("this");
+//         list.addFirst("is");
+//         list.printList();
+//         list.addFirst("linkedlist");
+//         list.printList();
+//         list.addLast("last");
+//         list.printList();
+//         list.DeleteFirst();
+//         list.printList();
+//         list.DeleteLast();
+//         list.printList();
+//         list.addFirst("Talha");
+//         list.printList();
+//         System.out.println(list.getSize());
+//     }
+// }
+
+import java.util.*;
 class LL{
-    Node head;
-    class Node{
-        String data;
-        Node next;
-        Node(String data){
-            this.data=data;
-            this.next=null;
+    public void main(String args[]){
+        LinkedList<String> list=new LinkedList<String>();
+        list.addFirst("Talha");
+        list.addFirst("student");
+        list.addFirst("the");
+        list.addFirst("I am");
+        list.addLast("Joiya");
+        System.out.println(list);
+        System.out.println(list.size());
+        for(int i=0;i<list.size();i++){
+            System.out.print(list.get(i)+"-->");
         }
-    }
-    
-    public void addFirst(String data){
-        Node newNode=new Node(data);
-        if(head==null){
-            newNode=head;
-            return;
-        }
-        newNode.next=head;
-        head=newNode;
-    }
-    
+        System.out.println("null");
 
-    public void addLast(String data){
-        Node newNode=new Node(data);
-        if(head==null){
-            newNode=head;
-            return;
-        }
-        Node curNode=head;
-        while(curNode.next!=null){
-            curNode.next=curNode;
-        }
-        curNode.next=newNode;
-    }
-    public static void main(String arg[]){
-        LL list=new LL();
-        list.addFirst("this");
-        list.addFirst("is");
-
+        list.removeFirst();
+        System.out.println(list);
+        list.removeLast();
+        System.out.println(list);
+        System.out.println(list.remove(0));
     }
 }
